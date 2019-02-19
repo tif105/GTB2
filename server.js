89473e0a83f19e498f582289bcf7168f79770038
tif105 <34311310+tif105@users.noreply.github.com>
2019-02-19 07:41:06 +0000

    SQL bug gix (close connection)

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/*💀 Christopher-+-Watson💀*/
+
 const http = require('http');
 const fs = require('fs');
 const qs = require('querystring');
@@ -5,10 +7,10 @@ const mysql = require('mysql');
 const mdhash = require('md5');
 
 const pid = process.pid;
+// load in HTML files read!  🌞
 var apphtml=(fs.readFileSync('./index.htm'));
 var jqueryfile=(fs.readFileSync('./jquery.js'));
 var loginhtml=(fs.readFileSync('login.html'));
-var hits=0;
 var calendarhtml=(fs.readFileSync("./calendar.htm"));
 
 const connection = mysql.createConnection({
@@ -33,7 +35,7 @@ async function tokenauthenticate(request){
         query="select * from tokens where session='"+session+"' and uuid='"+uuid+"';";
         user = new Promise(function (resolve,reject){
             connection.query(query, function (error, results,fields){
-                if (results.length>0){ //confirms a match has been found in the databse
+                if (results.length>0){ //confirms a match has been found in the database
                     console.log("authenticated");
                     authenticated=true;
                     resolve(results[0].username);
@@ -42,6 +44,7 @@ async function tokenauthenticate(request){
                     resolve("unauthenticated");
                 }
             });
+            connection.end();
         });
 
         return await user;
@@ -180,6 +183,7 @@ function airespond(req,res){
         var query="insert into goals (username,goal) values('"+req.username+"','"+goal+"');";
     }
     connection.query(query);
+    connection.end();
     res.end(goal);
 
 }
@@ -199,6 +203,7 @@ function addtogoals(req,res){
     var query="insert into goals (username,goal) values('"+req.username+"','"+goal+"');";
     console.log(query);
     connection.query(query);
+    connection.end();
     res.end();
 }
 
@@ -315,11 +320,13 @@ const app = function (req,res){
                             }
                         }
                     });
+                    connection.end();
                     break;
                 case "/deletegoal" :
                     ID=req.post['ID'];
                     query="delete from goals where ID='"+ID+"' and username='"+req.username+"';";
                     connection.query(query);
+                    connection.end();
                     res.end("success");
                     break;
     
@@ -340,6 +347,7 @@ const app = function (req,res){
                             }
                             res.end(goals);
                         });
+                        connection.end();
                     }else{
                         res.end("Authentication Error");
                     }
